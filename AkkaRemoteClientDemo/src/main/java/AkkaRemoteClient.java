675a83d700b587e227bc7913a0c164edd81c0ccf
unknown <akmula@IGLR000PB0RE7E.i.ameriprise.com>
2013-12-03 20:38:03 +0530

    Initial Commit

---
+++ AkkaRemoteClientDemo/src/main/java/AkkaRemoteClient.java
@@ -0,0 +1,84 @@
+
+
+import com.typesafe.config.ConfigFactory;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.actor.UntypedActor;
+
+public class AkkaRemoteClient {
+	
+	static class LocalActor extends UntypedActor {		
+		ActorRef remoteActor;
+		
+		@SuppressWarnings("deprecation")
+		@Override
+		public void preStart() {
+			//Get a reference to the remote actor
+			remoteActor = getContext().actorFor("akka.tcp://RemoteSys@127.0.0.1:2662/user/remoteActor");
+		}
+				
+		@Override
+		public void onReceive(Object message) throws Exception {			
+			/*if(obj instanceof Message) {
+				Message msg = (Message) obj;
+				if(msg.getMessageSender() == null) {
+					msg.setMessageSender(getSelf());
+					remoteActor.tell(msg, getSelf());
+					System.out.println("Me - " + msg.getMessageContents());
+				}else {
+					System.out.println(msg.getMessageSender() + " - " + msg.getMessageContents());
+				}				
+			}else
+				System.out.println("Alien message");*/
+			
+//			Timeout timeout = new Timeout(5, TimeUnit.SECONDS);
+			
+			if(message instanceof Message) {
+				Message msg = (Message) message;
+				if(msg.getMessageSender() == null) {
+					msg.setMessageSender(getSelf());
+					remoteActor.tell(msg, getSelf());
+				}else {
+					System.out.println(msg.getMessageContents());
+				}
+				/*Future<Object> future = Patterns.ask(remoteActor, message.toString(),timeout);
+				String result = (String) Await.result(future, timeout.duration());
+				System.out.println("Message received from Server ->" + result);*/
+			}
+		}
+	}
+	
+    public static void main( String[] args ) throws InterruptedException {
+    	
+    	ActorSystem _system = ActorSystem.create("LocalNodeApp",ConfigFactory.load().getConfig("LocalSys"));
+		ActorRef LocalActor = _system.actorOf(new Props(LocalActor.class));
+		
+		for(int i =0;i<1000;i++) {	
+			Thread.sleep(2000);
+			LocalActor.tell(new Message("this is message no " + i,null),ActorRef.noSender());
+		}
+		_system.shutdown();
+    	
+    	
+    	/*
+    	
+    	Scanner input = new Scanner(System.in);
+    	ActorSystem _system = ActorSystem.create("LocalNodeApp", ConfigFactory.load().getConfig("LocalSys"));
+    	
+		ActorRef LocalActor = _system.actorOf(new Props(LocalActor.class));
+		boolean stop = false;
+		String message = null;
+		while(!stop) {
+			//System.out.print("Me (send '_quit' to stop) - ");
+			message = input.nextLine();
+			if(message.equalsIgnoreCase("_quit"))
+				stop = true;
+			LocalActor.tell(new Message(message,ActorRef.noSender()), ActorRef.noSender());
+		}
+		
+		*/
+    }
+
+}
---
+++ RemoteEchoServer/src/main/java/Message.java
@@ -0,0 +1,26 @@
+
+
+import java.io.Serializable;
+import akka.actor.ActorRef;
+
+class Message implements Serializable {
+		
+	ActorRef messageSender;
+	String messageContents;
+	public Message(String messageContents, ActorRef messageSender) {
+		this.messageSender = messageSender;
+		this.messageContents = messageContents;
+	}
+	public ActorRef getMessageSender() {
+		return messageSender;
+	}
+	public void setMessageSender(ActorRef messageSender) {
+		this.messageSender = messageSender;
+	}
+	public String getMessageContents() {
+		return messageContents;
+	}
+	public void setMessageContents(String messageContents) {
+		this.messageContents = messageContents;
+	}
+}
