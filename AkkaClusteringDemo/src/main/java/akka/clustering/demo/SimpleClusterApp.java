4a584161c847df997e71f43a50b4311fb3dae99d
amitmula <amit.mula@ampf.com>
2013-12-18 18:44:26 +0530

    initial commit

---
+++ AkkaClusteringDemo/src/main/java/akka/clustering/demo/SimpleClusterApp.java
@@ -0,0 +1,27 @@
+package akka.clustering.demo;
+
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import akka.cluster.Cluster;
+import akka.cluster.ClusterEvent.ClusterDomainEvent;
+ 
+public class SimpleClusterApp {
+ 
+  public static void main(String[] args) {
+    // Override the configuration of the port
+    // when specified as program argument
+    if (args.length > 0)
+      System.setProperty("akka.remote.netty.tcp.port", args[0]);
+ 
+    // Create an Akka system
+    ActorSystem system = ActorSystem.create("ClusterSystem");//,ConfigFactory.load().getConfig("ClusterSystem"));
+	
+    // Create an actor that handles cluster domain events
+    ActorRef clusterListener = system.actorOf(Props.create(SimpleClusterListener.class), "clusterListener");
+ 
+    // Add subscription of cluster events
+    Cluster.get(system).subscribe(clusterListener, ClusterDomainEvent.class);
+  }
+  
+}
